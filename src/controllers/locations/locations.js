51f7715424fa3c3e742ab30117a0d20f76d5313d
Olga <olgadubinova8@gmail.com>
2026-09-26 18:08:52 +0300

    for merging

--- src/controllers/locations/locations.js
+++ src/controllers/locations/locations.js
@@ -4,8 +4,8 @@ export const createLocationController = async (req, res) => {
   const { name, type, region, description } = req.body;
   const { file, user } = req;
   if (!req.file) {
-    throw createHttpError(400, 'Image is required');
+    return res.status(400).json({ message: 'Image file is required' });
   }
   const id = user._id;
-  const result = LocationModel.create;
+  const result = LocationModel.create({});
 };
